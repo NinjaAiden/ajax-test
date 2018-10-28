5b623bdc6fbb1490469e0916eac3745548cd4c0b
Aiden Harvey <aidenharvey102@gmail.com>
2018-10-28 08:21:06 +0000

    deserialised data as a JSON object

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var xhr = new XMLHttpRequest();
 
+//define informaiton we want to recieve
+xhr.open("GET", "https://swapi.co/api/");
+//send the request
+xhr.send();
+
+function setData(jsonData){
+    data = jsonData;
+}
+
 xhr.onreadystatechange = function(){
-    if(this.readyState = 4 && this.status == 200){
-        document.getElementById("data").innerHTML = this.responseText;
+    if(this.readyState == 4 && this.status == 200){
+        setData(JSON.parse(this.responseText));
     }
 };
-
-xhr.open("GET", "https://swapi.co/api/");
-
-xhr.send()
