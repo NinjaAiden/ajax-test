56b9acee3c677a474cbb2ca4d8673cca56df3f79
Aiden Harvey <aidenharvey102@gmail.com>
2018-10-28 08:27:58 +0000

    added a timeout callback to assist in data loading

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 var xhr = new XMLHttpRequest();
+var data;
 
 //define informaiton we want to recieve
 xhr.open("GET", "https://swapi.co/api/");
 //send the request
 xhr.send();
 
-function setData(jsonData){
-    data = jsonData;
-}
-
 xhr.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-        setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
     }
 };
+
+setTimeout(function(){
+    console.log(data);
+}, 1000);
