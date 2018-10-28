a18471d3c58cdbff3a7d8a07fe0aa20d80325383
Aiden Harvey <aidenharvey102@gmail.com>
2018-10-28 08:37:52 +0000

    added callback function to define when data gets loaded

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-var xhr = new XMLHttpRequest();
-var data;
+function getData(cb) {
+    
+    var xhr = new XMLHttpRequest();
 
-//define informaiton we want to recieve
-xhr.open("GET", "https://swapi.co/api/");
-//send the request
-xhr.send();
+    //define informaiton we want to recieve
+    xhr.open("GET", "https://swapi.co/api/");
+    //send the request
+    xhr.send();
 
-xhr.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        data = JSON.parse(this.responseText);
-    }
-};
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
 
-setTimeout(function(){
+function printDataToConsole(data){
     console.log(data);
-}, 1000);
+}
+
+getData(printDataToConsole);
